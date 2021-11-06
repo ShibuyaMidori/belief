eb5930291ebfb60a13013c1916449d984b0e8ccf
ShibuyaMidori <ShibuyaMidori@mail.com>
2021-11-06 19:37:03 +0900

    :rotating_light: Widgetテスト を追加した

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -2,15 +2,14 @@ import 'package:belief/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
-  test('dummy', () {
-    var ans = dummy_func(2);
-    expect(ans, 3);
+  test('dummy 1', () {
+    var ans1 = dummy_func(2);
+    expect(ans1, 3);
   });
 
   test('dummy 2', () {
-    var ans = dummy_func(2);
-    ans = dummy_func(ans);
-    expect(ans, 4);
+    var ans2 = dummy_func(2);
+    ans2 = dummy_func(ans2);
+    expect(ans2, 4);
   });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,19 +5,23 @@ import 'package:belief/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // アプリを起動
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
+    // 0が書かれているオブジェクトが存在することを確認する
     expect(find.text('0'), findsOneWidget);
+    // 1が書かれているオブジェクトが存在しないことを確認する
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // 追加ボタンを2回タップ
     await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    // タップした後画面を更新
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // 0が書かれているオブジェクトが存在しないことを確認する
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // 2が書かれているオブジェクトが存在することを確認する
+    expect(find.text('2'), findsOneWidget);
   });
 }
